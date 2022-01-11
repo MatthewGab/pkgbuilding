39dca54bb83689af4cd91c4d7bd5953ff59ab5ac
MatthewGab <matthew@dgnet.cloud>
2022-01-11 12:15:01 +0800

    Add custom repo name argument

--- upload_pkgs.sh
+++ upload_pkgs.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-if [[ ! -n "$1" ]]; then
-    echo "No specified username"
+if [[ ! -n "$1" && ! -n "$2" ]]; then
+    echo "Please specify a user & repo name"
     exit 1
 fi
 
 user="$1@pkgserver"
-path="/mnt/packages_scrapped/archlinux/synergy/os/x86_64/"
+repo="$2"
+repo_path="./repo/os/x86_64"
+path="/mnt/packages_scrapped/archlinux/$repo/os/x86_64/"
 
 # create repo db files
-(cd ./synergy/os/x86_64/ ; 
-repo-add synergy.db.tar.gz *.pkg.tar.zst ;
+(cd ${repo_path} ; 
+repo-add $repo.db.tar.gz *.pkg.tar.zst ;
 rm -rf *.old)
 
-# transfer packages & db files to synergy package server
-scp ./synergy/os/x86_64/* ${user}:${path}
+# transfer packages & db files to package server
+scp ${repo_path}/* ${user}:${path}
+
+# Remove all files in local dir: os/x86_64
+rm -rf ${repo_path}/*
