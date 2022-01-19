d8177e6992f593a32c1c24a3ee88b69b6404b986
MatthewGab <matthew@dgnet.cloud>
2022-01-19 10:25:54 +0800

    Update program IDs list

--- programs/get-programs.sh
+++ programs/get-programs.sh
@@ -8,20 +8,40 @@ c_reset="\033[0m"
 
 # Github Personal Token
 _token=""
-_url="https://dgcrepotool:$_token@github.com/DamianoGlobal"
+_url="https://dgcrepotool:${_token}@github.com/DamianoGlobal"
 
-# DGnet Program Repositories
-declare -a repositories=(
-ID20
-ID37
-ID43
+# DGnet Program IDs
+declare -a IDs=(
+1
+14
+15
+19
+2
+20
+21
+22
+29
+3
+33
+35
+37
+38
+39
+4
+40
+41
+42
+43
+5
+7
+8
 )
 
-# Clones everything in the repositories list
-for repo in ${repositories[@]}; do
-    if [[ ! -d "DGnet_Program_$repo" ]]; then
-        git clone ${_url}"/DGnet_Program_"${repo}
+# Clones Program Repositories in the IDs list
+for ID in ${IDs[@]}; do
+    if [[ ! -d "DGnet_Program_$ID" ]]; then
+        git clone ${_url}"/DGnet_Program_ID"${ID}
     else
-        printf "${c_red}DGnet_Program_$repo already cloned.${c_reset}\n"
+        printf "${c_red}DGnet_Program_ID${ID} already cloned.${c_reset}\n"
     fi
 done
