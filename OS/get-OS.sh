b10c50bbbbdc1f499686a6fa4d9f3ef11f5dc0ce
MatthewGab <matthew@dgnet.cloud>
2022-01-20 11:04:03 +0800

    Add `OS` to clone `DGnet_Operating_System` repositories

---
+++ OS/get-OS.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# colors
+c_red="\033[1;31m"
+c_green="\033[1;32m"
+c_blue="\033[1;34m"
+c_reset="\033[0m"
+
+# Github Personal Token
+_token="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"
+_url="https://dgcrepotool:$_token@github.com/DamianoGlobal"
+
+# DGnet Product Repositories
+declare -a systems=(
+1
+2
+)
+
+# Clones everything in the repositories list
+for OS in ${systems[@]}; do
+    if [[ ! -d "DGnet_Operating_System$OS" ]]; then
+        git clone ${_url}"/DGnet_Operating_System"${OS}
+    else
+        printf "${c_red}DGnet_Operating_System$OS already cloned.${c_reset}\n"
+    fi
+done
