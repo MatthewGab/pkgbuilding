ffd8517ac3bc0bb99c40ef1c6e2d65243496f834
MatthewGab <matthew@dgnet.cloud>
2022-01-20 10:30:37 +0800

    Improve `build.sh` script

--- ALP/build.sh
+++ ALP/build.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
+# colors
+c_red="\033[1;31m"
+c_green="\033[1;32m"
+c_blue="\033[1;34m"
+c_reset="\033[0m"
+
 # create path if not exists
 repo_pkg_path="repo/os/x86_64"
 if [[ ! -d "../$repo_pkg_path" ]]; then
     mkdir -p "../$repo_pkg_path"
 fi
 
-for i in ./*/; do 
-    (cd "$i" ;
-    git checkout dev-stable && git pull ;
+for i in ./DGnet_ALP_*/; do
 
-    makepkg --skipinteg \
-            --skipchecksums \
-            --skippgpcheck \
-            --syncdeps \
-            --force \
-            --clean \
-            --noconfirm ; 
+    if [[ -f ${i}"/PKGBUILD" ]]; then
+        (printf "\n\n${c_green}$(basename "$i" | sed 's/DGnet_ALP_//g')${c_reset}\n"
+
+        #figlet $(basename "$i" | sed 's/DGnet_ALP_//g' | lolcat ;
+        cd "$i" ;
     
-    mv *.pkg.tar.zst ../../${repo_pkg_path})
+        #git checkout dev-stable &&
+        git pull ;
+
+        makepkg --skipinteg \
+                --skipchecksums \
+                --skippgpcheck \
+                --syncdeps \
+                --force \
+                --clean \
+                --noconfirm ;
+
+        mv *.pkg.tar.zst ../../${repo_pkg_path})
+    else
+
+      printf "\n\n${c_red}[!] No PKGBUILD file found in $(basename "$i")${c_reset}\n"
+    fi
 done
