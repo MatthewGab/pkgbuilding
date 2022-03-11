6fdd39b57927d647c137b8b9b77383c1b918dfc0
MatthewGab <matthew@dgnet.cloud>
2022-03-11 01:44:03 -0500

    Simplified the script

--- build-n-upload.sh
+++ build-n-upload.sh
@@ -4,18 +4,6 @@ set -e
 ALP=$(pwd)"/ALP"
 AUR=$(pwd)"/3rdparty"
 
-build_ALP() {
-	(cd ${ALP}; ./build.sh)
-}
-
-build_AUR() {
-	(cd ${AUR}; ./auto-build.sh)
-}
-
-upload_pkgs() {
-	./upload_pkgs.sh pkgupdater pkgtest
-}
-
-build_ALP
-build_AUR
-upload_pkgs
+(cd ${ALP}; ./build.sh)
+(cd ${AUR}; ./auto-build.sh)
+./upload_pkgs.sh pkgupdater pkgtest
