284b33aefb5886397159852307e7fe752c426ae3
MatthewGab <matthew@dgnet.cloud>
2022-03-13 23:23:36 -0400

    [Update]: download package repository and updates it.

--- upload_pkgs.sh
+++ upload_pkgs.sh
@@ -8,15 +8,20 @@ fi
 user="$1@pkgserver"
 repo="$2"
 repo_path="./repo/os/x86_64"
-path="/mnt/packages_scrapped/archlinux/$repo/os/x86_64/"
+server_repo="./${repo}/os/x86_64"
+path="/mnt/packages_scrapped/archlinux/$repo"
 
-# create repo db files
-(cd ${repo_path} ; 
+# get current package repository from server
+scp -r ${user}:${path} .
+
+# update packages and database files
+(mv ${repo_path}/* ${server_repo} ;
+cd ${server_repo} ;
 repo-add $repo.db.tar.gz *.pkg.tar.zst ;
-rm -rf *.old)
+rm -f *.old)
 
 # transfer packages & db files to package server
-scp ${repo_path}/* ${user}:${path}
+scp ${server_repo}/* ${user}:${path}/os/x86_64
 
 # Remove all files in local dir: os/x86_64
-rm -rf ${repo_path}/*
+rm -rf ${repo_path} ${repo}
