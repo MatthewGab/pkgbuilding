1248c20557af33019515e4ad10cc11915c5f98c9
MatthewGab <matthew@dgnet.cloud>
2022-04-11 21:34:02 -0400

    feat: add counting of packages to make sure packages are complete before uploading to pakcage server.

--- build-n-upload.sh
+++ build-n-upload.sh
@@ -7,4 +7,20 @@ AUR=$(pwd)"/3rdparty"
 
 (cd ${ALP}; ./status-repos.sh && ./build.sh)
 (cd ${AUR}; ./auto-build.sh)
+
+if [ $(./pkgcount) -eq 0 ]; then
+	echo ""
+	echo "######################################"
+	echo "##### Package Count Complete!... #####"
+	echo "######################################"
+	echo ""
+else
+	echo ""
+	echo "########################################"
+	echo "##### Package Count Incomplete!... #####"
+	echo "########################################"
+	echo ""
+	exit 1
+fi
+
 ./upload_pkgs.sh pkgupdater pkgtest
